f0ac2d76e148924c4cf36b7eb6742bb8b0efa6d5
Liam Stabeno <liamstabeno@gmail.com>
2017-05-11 09:04:23 -0700

    complete project upto firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyCgiJaJ1DyuJUHG8SzxEs4lb4PdgAPIqUI",
+  authDomain: "online-store-85bcf.firebaseapp.com",
+  databaseURL: "https://online-store-85bcf.firebaseio.com",
+  projectId: "online-store-85bcf",
+  storageBucket: "online-store-85bcf.appspot.com",
+  messagingSenderId: "447232947338"
+ };
